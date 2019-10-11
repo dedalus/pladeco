dd3a2bdf5bf210614579cc7bbb5a35fc96b2da29
Elio Viola <elio.13v@gmail.com>
2019-10-11 01:27:09 -0300

    Model improvments.

--- Model/Area.cs
+++ Model/Area.cs
@@ -9,7 +9,7 @@ namespace Model
     {
         [Key]
         public int ID { get; set; }
-        public string Description { get; set; }
+        public string Name { get; set; }
         public bool Active { get; set; }
     }
 }
